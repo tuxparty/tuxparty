f582ac281c67e4f60859788930fd025c3e944f6b
Colin Reeder <vpzomtrrfrt@gmail.com>
2017-11-13 22:43:40 -0700

    Initial work on JoinState

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod game;
 mod tputil;
+extern crate gilrs;
 extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 extern crate sdl2_window;
-extern crate gilrs;
 
 use sdl2_window::Sdl2Window as Window;
 use piston::input::{RenderEvent, UpdateEvent};
@@ -13,17 +13,17 @@ fn main() {
     let gl_version = opengl_graphics::OpenGL::V3_2;
 
     let mut window: Window = piston::window::WindowSettings::new("Tuxparty", [800, 600])
-    .opengl(gl_version)
-    .exit_on_esc(true)
-    .srgb(false)
-    .build()
-    .unwrap();
+        .opengl(gl_version)
+        .exit_on_esc(true)
+        .srgb(false)
+        .build()
+        .unwrap();
 
     let mut gl = opengl_graphics::GlGraphics::new(gl_version);
 
     let mut app = game::App {
         input: tputil::InputState::new(),
-        state: Some(Box::new(MenuState{}))
+        state: Some(Box::new(MenuState {})),
     };
 
     let mut events = piston::event_loop::Events::new(piston::event_loop::EventSettings::new());
@@ -43,28 +43,52 @@ struct MenuState {}
 impl game::State for MenuState {
     fn render(&self, gl: &mut opengl_graphics::GlGraphics, trans: graphics::math::Matrix2d) {
         const COLOR1: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-        graphics::rectangle(COLOR1, graphics::rectangle::centered_square(0.0, 0.0, 0.1), trans, gl);
+        graphics::rectangle(
+            COLOR1,
+            graphics::rectangle::centered_square(0.0, 0.0, 0.1),
+            trans,
+            gl,
+        );
     }
     fn update(&mut self, app: &mut game::App, time: f64) {
-        if app.input.get_pressed_any(tputil::Button::South).len() > 0 {
-            app.goto_state(JoinState::new());
+        let pressed = app.input.get_pressed_any(tputil::Button::South);
+        if pressed.len() > 0 {
+            app.goto_state(JoinState::new(pressed[0]));
         }
     }
 }
 
-struct JoinState {}
+struct JoinState {
+    players: Vec<usize>,
+}
 
 impl JoinState {
-    fn new() -> JoinState {
-        return JoinState {};
+    fn new(player: usize) -> JoinState {
+        let mut tr = JoinState {
+            players: Vec::new()
+        };
+        tr.players.push(player);
+        return tr;
     }
 }
 
 impl game::State for JoinState {
     fn render(&self, gl: &mut opengl_graphics::GlGraphics, trans: graphics::math::Matrix2d) {
-        
+        const COLOR2: [f32; 4] = [0.0, 1.0, 1.0, 1.0];
+        let count = self.players.len();
+        let scale = 2.0 / (count + 1) as f64;
+        for i in 0..count {
+            graphics::rectangle(
+                COLOR2,
+                graphics::rectangle::centered_square(
+                    scale * (i + 1) as f64 - 1.0,
+                    0.0,
+                    scale / 4.0,
+                ),
+                trans,
+                gl,
+            );
+        }
     }
-    fn update(&mut self, app: &mut game::App, time: f64) {
-
-    }
+    fn update(&mut self, app: &mut game::App, time: f64) {}
 }
